5f83038cafde6ee0dfe0b53e8e2f64df30fd1fbb
Ольга Головкина <GolovkinaOS@bimacad.ru>
2024-10-25 22:25:24 +0300

    Реализация запуска и настройки окна

---
+++ NCadAvaloniaSample.Avalonia/AvaloniaBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+
+namespace NCadAvaloniaSample;
+
+public static class AvaloniaBuilder
+{
+    public static bool WasntBuilt { get; private set; } = true;
+
+    public static AppBuilder BuildApp()
+    {
+        var builder = AppBuilder.Configure<App>()
+                                .UsePlatformDetect()
+                                .WithInterFont()
+                                .SetupWithoutStarting();
+
+        WasntBuilt = false;
+
+        return builder;
+    }
+}
---
+++ NCadAvaloniaSample.Avalonia/Program.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using System;
+
+namespace NCadAvaloniaSample.Avalonia;
+
+
+/*
+ * Пусть будет филлером для случая, когда понадобится тестить окно без привязки к nanoCAD.
+ * Достаточно просто назначить проект в качестве запускаемого.
+ */
+public static class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp() => 
+        AppBuilder.Configure<App>()
+                  .UsePlatformDetect()
+                  .WithInterFont()
+                  .LogToTrace();
+}
